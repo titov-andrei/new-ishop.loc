1f32a9b709dea197e65f17368d95f6536589783d
titov-andrei <titov_andrei@mail.ru>
2022-06-09 16:14:45 +0300

    ...

---
+++ config/init.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/wfm');
+define("HELPERS", ROOT . '/vendor/wfm/helpers');
