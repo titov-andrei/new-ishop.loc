7e09528cd4a3c82307314b81934f9fec16f7bece
titov-andrei <titov_andrei@mail.ru>
2022-06-09 17:36:53 +0300

    Update init.php

--- config/init.php
+++ config/init.php
@@ -6,3 +6,12 @@ define("WWW", ROOT . '/public');
 define("APP", ROOT . '/app');
 define("CORE", ROOT . '/vendor/wfm');
 define("HELPERS", ROOT . '/vendor/wfm/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
+define("CONFIG", ROOT . '/config');
+define("LAYOUT", 'ishop');
+define("PATH", 'http://new-ishop.loc');
+define("ADMIN", 'http://new-ishop.loc/admin');
+define("NO_IMAGE", 'uploads/no_image.jpg');
+
+require_once ROOT . '/vendor/autoload.php';
