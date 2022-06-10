72e5dd3313679a4251aa6810927af575c01a28be
titov-andrei <titov_andrei@mail.ru>
2022-06-10 21:04:57 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+if (PHP_MAJOR_VERSION < 8) {
+  die('Необходима версия PHP >= 8');
+}
+
 require_once dirname(__DIR__) . '/config/init.php';
 
-echo 'test';
+echo "test" . "<br/>";
+
+echo PHP_MAJOR_VERSION;
